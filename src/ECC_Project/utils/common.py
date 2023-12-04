3377ce03842e2bfc34e567ded5c2e984e89af94b
Ellanki Dileepsai <ellankidileepsai@Ellankis-MacBook-Pro.local>
2023-12-04 14:11:20 -0500

    “updated”

--- src/ECC_Project/utils/common.py
+++ src/ECC_Project/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from ECC_Project import logger
+from src.ECC_Project import logger
 import json
 import joblib
 from ensure import ensure_annotations
