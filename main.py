a3aea94d1ecdd6c7a867369b0dc8174677094d0f
Ellanki Dileepsai <ellankidileepsai@Ellankis-MacBook-Pro.local>
2023-12-04 15:46:38 -0500

    stage2 done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.ECC_Project import logger
 from src.ECC_Project.pipeline.stage_01 import DataIngestionTrainingPipeline
+from src.ECC_Project.pipeline.stage_02 import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -12,3 +13,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
